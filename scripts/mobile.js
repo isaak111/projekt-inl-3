6fa250593a37e1df029f44ede552180fef64b306
Korvin1337 <korvin1337@gmail.com>
2021-11-04 14:56:38 +0100

    removed pictures that werent used, fixed javascript bug when switching between mobile and pc mode back and forth

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,3 +1,16 @@
 export function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Tablet|iPad/i.test(navigator.userAgent);
 };
+
+export function deviceType() {
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test((navigator.userAgent))) {
+        console.info("tablet");
+        return "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test((navigator.userAgent))) {
+        console.info("mobile");
+        return "mobile";
+    }
+    console.info("true");
+    return "desktop";
+};
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-import { isMobile } from './mobile.js';
+import { isMobile, deviceType } from './mobile.js';
 
 if (!isMobile()) {
     (function () {
@@ -6,22 +6,24 @@ if (!isMobile()) {
         let bodyList = ["home", "about", "services"];
 
         function displaySection(e) {
-            console.info("e.target.innerText = " + e.target.innerText);
-            if (e.target.innerText != "Contact" && e.target.innerText != null && e.target.innerText != "Home Services About Contact") {
-                for (let i = 0; i < bodyList.length; i++) {
-                    let element = document.getElementById(bodyList[i]);
-                    element.classList.add("hidden");
-                    if (!element.classList.contains("hero-section")) {
-                        element.classList.remove("flex");
+            if (!isMobile()) {
+                console.info("e.target.innerText = " + e.target.innerText);
+                if (e.target.innerText != "Contact" && e.target.innerText != null && e.target.innerText != "Home Services About Contact") {
+                    for (let i = 0; i < bodyList.length; i++) {
+                        let element = document.getElementById(bodyList[i]);
+                        element.classList.add("hidden");
+                        if (!element.classList.contains("hero-section")) {
+                            element.classList.remove("flex");
+                        }
                     }
-                }
-                let displaySection = document.getElementById(e.target.innerText.toLowerCase());
+                    let displaySection = document.getElementById(e.target.innerText.toLowerCase());
 
-                if (displaySection != null) {
-                    if (!displaySection.classList.contains("hero-section")) {
-                        displaySection.classList.add("flex");
+                    if (displaySection != null) {
+                        if (!displaySection.classList.contains("hero-section")) {
+                            displaySection.classList.add("flex");
+                        }
+                        displaySection.classList.remove("hidden");
                     }
-                    displaySection.classList.remove("hidden");
                 }
             }
         }
